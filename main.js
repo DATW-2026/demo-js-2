c1da92073330398584bf380aa564d7e952aedb57
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-02-11 15:00:07 +0100

    Add projects files, update css and make click addEventListener button

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello JS!');
+
+const buttonElement = document.querySelector('#theme-toggle');
+buttonElement.addEventListener('click', makeAny);
+
+function makeAny() {
+    console.log('Han hecho click');
+}
