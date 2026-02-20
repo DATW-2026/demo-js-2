5fced737494d0289517b5906aa51c0a82798cc09
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-02-20 13:28:11 +0100

    Update components example

--- 5_web_components/components/sample.js
+++ 5_web_components/components/sample.js
@@ -1,4 +1,4 @@
-export const sample = () => {
+export const sampleOld = () => {
     const selector = 'app-sample';
 
     const setTemplate = () => {
@@ -15,5 +15,36 @@ export const sample = () => {
     // outerHTML sustituye la custom tag <app-sample>
     if (document.querySelector(selector)) {
         document.querySelector(selector).outerHTML = setTemplate();
+    }
+};
+
+export const sample = () => {
+    const selector = 'app-sample';
+
+    const text = 'Sample';
+    const template = /*html*/ `
+        <div class="container" title="sample">
+            <p lang="es">${text}</p>
+            <button>Borrar</button>
+        </div>
+    `;
+
+    const createElement = () => {
+        const parentElement = document.createElement('parent');
+        parentElement.innerHTML = template;
+        const element = parentElement.firstElementChild;
+        return element;
+    };
+
+    const element = createElement();
+
+    element.querySelector('button').addEventListener('click', (event) => {
+        event.stopPropagation();
+        console.log('Borrando');
+    });
+
+    if (document.querySelector(selector)) {
+        // document.querySelector(selector).appendChild(element); -> (innerHTML)
+        document.querySelector(selector).replaceWith(element);
     }
 };
